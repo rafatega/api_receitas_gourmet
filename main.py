947dece0340910accd47ea9f0cf2d6d793a8b87d
RafaTega <rafael_tegazzini@hotmail.com>
2026-01-02 23:56:43 -0300

    comentários

--- main.py
+++ main.py
@@ -45,45 +45,48 @@ class Recipe(db.Model):
 def home():
     return 'Página inicial'
 
+
 @app.route("/checkusers", methods=["GET"])
 def checkusers():
-  """
-  Checa todas informações dos usuários cadastrados
-  ---
-  """
-  users = User.query.all()
-  return jsonify([{"id": u.id, "username": u.username, "password": u.password} for u in users]), 200
+    """
+    Checa todas informações dos usuários cadastrados
+    ---
+    """
+    users = User.query.all()
+    return jsonify([{"id": u.id, "username": u.username, "password": u.password} for u in users]), 200
+
 
 @app.route('/register', methods=['POST'])
 def register_user():
-  """
-  Registra um novo usuário.
-  ---
-  parameters:
-    - in: body
-      name: body
-      required: true
-      schema:
-        type: object
-        properties:
-          username:
-            type: string
-          password:
-            type: string
-  responses:
-    201:
-      description: Usuário criado com sucesso
-    400:
-      description: Usuário já existe
-  """
+    """
+    Registra um novo usuário.
+    ---
+    parameters:
+      - in: body
+        name: body
+        required: true
+        schema:
+          type: object
+          properties:
+            username:
+              type: string
+            password:
+              type: string
+    responses:
+      201:
+        description: Usuário criado com sucesso
+      400:
+        description: Usuário já existe
+    """
 
-  data = request.get_json()
-  if User.query.filter_by(username=data['username']).first():
-      return jsonify({'message': 'Usuário já existe'}), 400
-  new_user = User(username=data['username'], password=data['password'])
-  db.session.add(new_user)
-  db.session.commit()
-  return jsonify({'message': 'Usuário criado com sucesso'}), 201
+    data = request.get_json()
+    if User.query.filter_by(username=data['username']).first():
+        return jsonify({'message': 'Usuário já existe'}), 400
+    new_user = User(username=data['username'], password=data['password'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'Usuário criado com sucesso'}), 201
+
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -116,14 +119,18 @@ def login():
         return jsonify({"access_token": token}), 200
     return jsonify({"error": "Invalid credential"}), 401
 
+
 @app.route("/protected", methods=["GET"])
 @jwt_required()
 def protected():
-    current_user_id = get_jwt_identity() # Retorna a identidade usada na criação do token
+    # Retorna a identidade usada na criação do token
+    current_user_id = get_jwt_identity()
     return jsonify({"msg": f"Usuário com ID {current_user_id} acessou a rota protegida"}), 200
+
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         print('Banco de dados criado!')
     app.run(debug=True)
+# Inicializa o banco de dados
